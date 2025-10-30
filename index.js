905e4973a4357d8c8cc966a333b05f929b9f509f
kiyotaki4 <marlinezxc@gmail.com>
2025-10-30 13:58:56 +0900

    develop danebaum pushed 3  files and actually made all

---
+++ index.js
@@ -0,0 +1,34 @@
+const button = document.querySelector('.main_btn')
+const input = document.querySelector('.main_input')
+const java_div = document.querySelector('.java_div')
+
+
+button.addEventListener('click',async()=>{
+    const input_value = input.value.trim()
+    if(input_value === ''){
+        return
+    }
+    java_div.innerHTML = ''
+    input.value = ''
+
+    const p = document.createElement('div')
+    p.textContent = 'загрузка...'
+    java_div.appendChild(p)
+
+    const temp = await getWeather(input_value)
+    const city = await getCity(input_value)
+     p.textContent = `в ${city} ощущается как: ${temp}°C`
+})
+
+async function getWeather(city){
+    const res = await fetch(`https://wttr.in/${encodeURIComponent(city)}?format=j1`)
+    const data = await res.json()
+    return data.current_condition[0].FeelsLikeC
+    // console.log(data);
+}
+async function getCity(city){
+    const res = await fetch(`https://wttr.in/${encodeURIComponent(city)}?format=j1`)
+    const data = await res.json()
+    return data.nearest_area[0].areaName[0].value
+    // console.log(data);
+}
