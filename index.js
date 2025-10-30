5a239db120019aa7106706b540591189c6d3a146
kiyotaki4 <marlinezxc@gmail.com>
2025-10-30 14:16:50 +0900

    develop upd index.js

--- index.js
+++ index.js
@@ -2,24 +2,36 @@ const button = document.querySelector('.main_btn')
 const input = document.querySelector('.main_input')
 const java_div = document.querySelector('.java_div')
 
+window.addEventListener('load',()=>{
+    console.log('322')
+    const sessionCity = localStorage.getItem('city'||null)
+    input.value = sessionCity
+    renderApp(sessionCity)
+
+})
 
 button.addEventListener('click',async()=>{
     const input_value = input.value.trim()
     if(input_value === ''){
         return
     }
+    saveCity(input_value)
+
     java_div.innerHTML = ''
     input.value = ''
+    renderApp(input_value)
 
-    const p = document.createElement('div')
+   
+})
+async function renderApp(input_value){
+     const p = document.createElement('div')
     p.textContent = 'загрузка...'
     java_div.appendChild(p)
 
     const temp = await getWeather(input_value)
     const city = await getCity(input_value)
      p.textContent = `в ${city} ощущается как: ${temp}°C`
-})
-
+}
 async function getWeather(city){
     const res = await fetch(`https://wttr.in/${encodeURIComponent(city)}?format=j1`)
     const data = await res.json()
@@ -31,4 +43,7 @@ async function getCity(city){
     const data = await res.json()
     return data.nearest_area[0].areaName[0].value
     // console.log(data);
+}
+function saveCity(city){
+    localStorage.setItem('city',city)
 }
